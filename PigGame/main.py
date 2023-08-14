4196b2c43d70e19bc6a8cbecdf68416b90b45960
rmhass015 <rmhass015@gmail.com>
2023-08-14 12:46:43 -0400

    added while loop to check player count input

--- PigGame/main.py
+++ PigGame/main.py
@@ -7,5 +7,14 @@ def roll():
 
     return roll
 
-value = roll()
-print(value)
+while True:
+  players = input("Enter the number of players (1-4): ")
+  if players.isdigit():
+    players = int(players)
+    if 1 <= players <= 4:
+       break
+    else:
+       print("Invalid, try again.")
+    
+
+print(players)
