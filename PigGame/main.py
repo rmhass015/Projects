53b3007eaced2ab8e628947bccb33244100c31fe
rmhass015 <rmhass015@gmail.com>
2023-08-14 13:25:52 -0400

    finished the rest of game

--- PigGame/main.py
+++ PigGame/main.py
@@ -8,13 +8,44 @@ def roll():
     return roll
 
 while True:
-  players = input("Enter the number of players (1-4): ")
+  players = input("Enter the number of players (2 - 4): ")
   if players.isdigit():
     players = int(players)
-    if 1 <= players <= 4:
+    if 2 <= players <= 4:
        break
     else:
-       print("Invalid, try again.")
-    
+       print("Must be between 2 - 4 players.")
+  else:
+    print("Invalid, try again.")
 
-print(players)
+max_score = 50
+player_scores = [0 for _ in range(players)]
+
+while max(player_scores) < max_score:
+   for player_index in range(players):
+      print(f"Player {player_index + 1} turn has started!")
+      print(f"Your current score is: {player_scores[player_index]} \n")
+      current_score = 0
+
+      while True:
+        should_roll = input("Would you like to roll (y)? ")
+        if should_roll.lower() != "y":
+            break
+        
+        value = roll()
+        if value == 1:
+            print("You rolled a 1! Turn is OVER!")
+            current_score = 0
+            break
+        else:
+            current_score += value
+            print(f"You rolled a: {value}")
+
+        print(f"Your score is: {current_score}")
+
+      player_scores[player_index] += current_score
+      print(f"Your total score is: {player_scores[player_index]}")
+
+max_score = max(player_scores)
+winning_index = player_scores.index(max_score)
+print(f"Player {winning_index + 1} is the winner with the score of {max_score}.")
